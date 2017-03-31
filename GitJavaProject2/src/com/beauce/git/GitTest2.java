cf54ee1b262ace3a0f3258d45b32e32be4aeaaea
k0rg0th <barbaria.korgoth@gmail.com>
2017-03-31 23:31:40 +0200

    add a second line in main

--- GitJavaProject2/src/com/beauce/git/GitTest2.java
+++ GitJavaProject2/src/com/beauce/git/GitTest2.java
@@ -1,5 +1,8 @@
 package com.beauce.git;
 
 public class GitTest2 {
-
+	public static void main(String[] args) {
+		System.out.println("start GitTest2");
+		System.out.println("end GitTest2");
+	}
 }
